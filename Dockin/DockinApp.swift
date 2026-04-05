0c0287edc1fa8691e2d28c97357bb05d9fce5f3e
jingbenzhengyi <wht@jingbenzhengyi.com>
2026-04-05 12:56:56 +0800

    Initial Commit

---
+++ Dockin/DockinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockinApp.swift
+//  Dockin
+//
+//  Created by Hector on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DockinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
